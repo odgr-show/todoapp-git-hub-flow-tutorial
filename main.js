f00f790eddb78be1639607852a01f857dc02735f
odgr-show <121861788+odgr-show@users.noreply.github.com>
2023-08-02 21:34:21 +0900

    add function todo-add done

---
+++ main.js
@@ -0,0 +1,25 @@
+const addTodo = document.querySelector("#addTodo");
+const input = document.querySelector("#input");
+const todoList = document.querySelector("#todoList");
+
+addTodo.addEventListener("click", () => {
+    const inputTodo = document.querySelector("#input").value;
+    const li = document.createElement("li");
+    li.innerHTML = `<input type='checkbox'>${inputTodo}`;
+    todoList.appendChild(li);
+
+    input.value = "";
+});
+
+// document.addEventListener("change", function (e) {
+//     const target = e.target;
+//     if (target.tagName === "INPUT" && target.type === "checkbox") {
+//         if (target.checked) {
+//             target.parentNode.style.textDecoration = "line-through";
+//             target.parentNode.style.color = "#ddd";
+//         } else {
+//             target.parentNode.style.textDecoration = "none";
+//             target.parentNode.style.color = "#000";
+//         }
+//     }
+// });
